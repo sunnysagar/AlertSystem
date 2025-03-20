8d675b13172b102ce2d466c918b3ad528a98234d
Pranjul Pandey <pranjulpand3@gmail.com>
2025-03-20 12:20:39 +0530

    change in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,50 @@
 import React from "react";
+import { FaGoogle } from "react-icons/fa"; // Import Google Icon
 import "../styles/Login.css"; // Import CSS file
 
 const Login = () => {
   return (
     <div className="login-container">
-      {/* Left Section: Login Form */}
-      <div className="login-form">
-        <div className="logo">🔷</div>
-        <h2>Welcome back !</h2>
-        <p>Enter to get unlimited access to data & information.</p>
-
-        <form>
-          <label>Email *</label>
-          <input type="email" placeholder="Enter your mail address" required />
-
-          <label>Password *</label>
-          <div className="password-field">
-            <input type="password" placeholder="Enter password" required />
-            <span className="eye-icon">👁️</span>
+      { /* Left Section: Login Form  */}
+        <div className="login-form">
+          <div className="logo">
+            <img src="C:\Users\pranj\Desktop\Alert Generator - Photos\CI.png" alt="Company Logo" />
           </div>
+          <h2>Welcome back !</h2>
+          <p>Enter to get unlimited access to data & information.</p>
 
-          <div className="remember-forgot">
-            <label>
-              <input type="checkbox" /> Remember me
-            </label>
-            <a href="#">Forgot your password?</a>
-          </div>
+          <form>
+            <label>Email *</label>
+            <input type="email" placeholder="Enter your e-mail " required />
 
-          <button className="login-btn">Log In</button>
-        </form>
+            <label>Password *</label>
+            <div className="password-field">
+          <input type="password" placeholder="Enter password" required />
+          <span className="eye-icon">👁️</span>
+            </div>
 
-        <div className="or-divider">Or, Login with</div>
+            <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Remember me
+          </label>
+          <a href="#">Forgot your password?</a>
+            </div>
 
-        <button className="google-btn">
-          <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google" />
-          Sign up with Google
-        </button>
+            <button className="login-btn">Log In</button>
+          </form>
 
-        <p className="register-link">
-          Don't have an account? <a href="#">Register here</a>
-        </p>
-      </div>
+          <div className="or-divider">Or, Login with</div>
 
-      {/* Right Section: Decorative Panel */}
+          <button className="google-btn">
+          <FaGoogle className="google-icon" /> Sign up with Google
+          </button>
+
+          <p className="register-link">
+            Don't have an account? <a href="#">Register here</a>
+          </p>
+        </div>
+
+        {/* Right Section: Decorative Panel */}
       <div className="login-decor">
         {/* This div will be used for background design */}
       </div>
