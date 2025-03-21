9ffdec448c0ac4fa5085cdf6e05ee4c6bb7b98ec
Sunny <sunnysagar620@gmail.com>
2025-03-21 12:59:48 +0530

    commit the signup auth resolve

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,29 +4,40 @@ import "../styles/Register.css"; // Import CSS file for styling
 import { useAuth } from "../context/AuthContext";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [department, setDepartment] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [user, setUser] = useState({
+    name: '', 
+    department:'',
+    email:'',
+    password:''
+  })
   const [error, setError] = useState(null);
-  const navigate = useNavigate();
   const { signup } = useAuth();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    signup({ name, department, email, password });
-    
-    if (password !== confirmPassword) {
-      setError("Passwords do not match");
-      return;
+    try {
+      if (user.password !== confirmPassword) {
+        setError("Passwords do not match");
+        return;
+      }
+      signup(user);
+       // Here, send data to backend (API request for user registration)
+    //    console.log("user", user); 
+    // console.log("User Registered:", { name, department, email, password });
+      alert("Registration successful");
+    } catch (error) {
+      setError("Registration failed", error);
     }
 
-    // Here, send data to backend (API request for user registration)
-    console.log("User Registered:", { name, department, email, password });
+  };
 
-    // Redirect to login after successful registration
-    navigate("/login");
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    setUser((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
   return (
@@ -39,19 +50,42 @@ const Register = () => {
 
         <form onSubmit={handleSubmit}>
           <label>Name *</label>
-          <input type="text" placeholder="Enter your full name" required value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" 
+                 placeholder="Enter your full name" 
+                 name="name"
+                 required 
+                 value={user.name} 
+                 onChange={handleChange} />
 
           <label>Department *</label>
-          <input type="text" placeholder="Enter your department" required value={department} onChange={(e) => setDepartment(e.target.value)} />
+          <input type="text" 
+                 placeholder="Enter your department"  
+                  name="department"
+                 value={user.department} 
+                 onChange={handleChange} />
 
           <label>Email *</label>
-          <input type="email" placeholder="Enter your email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="email" 
+                 placeholder="Enter your email" 
+                  name="email"
+                 required 
+                 value={user.email} 
+                 onChange={handleChange} />
 
           <label>Password *</label>
-          <input type="password" placeholder="Enter password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" 
+                placeholder="Enter password" 
+                name="password"
+                required 
+                value={user.password} 
+                onChange={handleChange} />
 
           <label>Confirm Password *</label>
-          <input type="password" placeholder="Confirm password" required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+          <input type="password" 
+                  placeholder="Confirm password" 
+                  required 
+                  value={confirmPassword} 
+                  onChange={(e) => setConfirmPassword(e.target.value)} />
 
           <button className="register-btn" type="submit">Register</button>
         </form>
